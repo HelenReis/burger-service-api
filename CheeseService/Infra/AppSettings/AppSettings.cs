5fe5c7b2b53979ac07a990da2b7677a400f3d137
Helen Alves dos Reis <helen.reis@mrv.com.br>
2023-05-18 19:29:27 -0300

    Removing unused variables

--- CheeseService/Infra/AppSettings/AppSettings.cs
+++ CheeseService/Infra/AppSettings/AppSettings.cs
@@ -10,10 +10,7 @@ namespace CheeseService.Infra
             _config = config;
         }
         public string CheeseQueue => _configVariables.GetValue<string>("CheeseQueue");
-        public string CheeseClient => _configVariables.GetValue<string>("CheeseClient");
         public string RabbitMqHost => _config.GetValue<string>("RABBITMQ_HOST");
-        public string ExchangeCooking => _configVariables.GetValue<string>("ExchangeCooking");
         public string ExchangeClient => _configVariables.GetValue<string>("ExchangeClient");
-        public string TypeExchangeCooking => _configVariables.GetValue<string>("TypeExchangeCooking");
     }
 }
--- CheeseService/Infra/AppSettings/IAppSettings.cs
+++ CheeseService/Infra/AppSettings/IAppSettings.cs
@@ -3,10 +3,7 @@ namespace CheeseService.Infra
     public interface IAppSettings
     {
         string CheeseQueue { get; }
-        string CheeseClient { get; }
         string RabbitMqHost { get; }
-        string ExchangeCooking { get; }
         string ExchangeClient { get; }
-        string TypeExchangeCooking { get; }
     }
 }
