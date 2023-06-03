a676a284d5ba9a1fbc881c84a704c709e55ebfcb
Helen Alves dos Reis <helen.reis@mrv.com.br>
2023-06-03 10:49:59 -0300

    removing bread service

--- BreadService/Program.cs
+++
@@ -1,8 +0,0 @@
-using BreadService.Infra;
-using Microsoft.EntityFrameworkCore;
-using BreadService.Infra.Data;
-using BreadService.Application.Bread;
-using BreadService;
-
-var app = Startup.InitializeApp(args);
-app.Run();
